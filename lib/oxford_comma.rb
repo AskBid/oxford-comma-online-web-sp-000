6e3ceb56b9a1d9f3c0b44aea34744508c3d4a837
sergio marelli <sergio.mrll@gmail.com>
2019-11-03 21:54:20 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,18 +5,6 @@ def oxford_comma(array)
   last != '' ? "#{first}#{array.join(', ')} and #{last}" : first
 end
 
-# def oxford_comma(array)
-#   last = array.pop
-#   str = array.join(', ')
-#   str << (array.size > 0 ? " and #{last}" : last)
-# end
-# def oxford_comma(array)
-#   last = array.pop
-#   str = array.join(', ')
-#   puts 'this: '+str
-#   str << (array.size > 0 ? ", and #{last}" : last)
-# end
-
 puts oxford_comma(["kiwi", "durian", "starfruit", "mangos", "dragon fruits"]).inspect
 # puts '------'
 puts oxford_comma(["kiwi", "durian"]).inspect
