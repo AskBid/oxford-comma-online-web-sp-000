383034703d1122659f4c7e7bb9eabd42725b5be7
sergio marelli <sergio.mrll@gmail.com>
2019-11-03 21:28:35 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,22 @@
 def oxford_comma(array)
+  puts 'array: '+ array.inspect
   last = array.pop
+  puts 'array.pop: '+ array.inspect
+  puts 'last: '+ last
   str = array.join(', ')
-  puts 'this: '+str
+  puts 'str: '+ str
   str << (array.size > 0 ? ", and #{last}" : last)
 end
+# def oxford_comma(array)
+#   last = array.pop
+#   str = array.join(', ')
+#   puts 'this: '+str
+#   str << (array.size > 0 ? ", and #{last}" : last)
+# end
 
 puts oxford_comma(["kiwi", "durian", "starfruit", "mangos", "dragon fruits"]).inspect
+puts '------'
 puts oxford_comma(["kiwi", "durian"]).inspect
+puts '------'
 puts oxford_comma(["kiwi"]).inspect
+puts '------'
