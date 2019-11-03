08a1cb301b4db30eddc327cfdb99d506cb8fb7cb
sergio marelli <sergio.mrll@gmail.com>
2019-11-03 22:07:28 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,9 @@
 def oxford_comma(array)
-  extra_comma = array.size > 2 ? ', ' : ''
+  extra_comma = array.size > 2 ? ',' : ''
   first = array.shift
   # array.unshift('')
   last = array.pop
-  last != '' ? "#{first}#{extra_comma}#{array.join(', ')}#{extra_comma} and #{last}" : first
+  last ? "#{first}#{extra_comma}#{array.join(', ')}#{extra_comma} and #{last}" : first
 end
 
 # def oxford_comma(array)
