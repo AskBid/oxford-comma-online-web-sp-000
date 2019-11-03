273ec02c89b3083db41ad3db0997bba248174d1e
sergio marelli <sergio.mrll@gmail.com>
2019-11-03 21:35:14 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,7 +6,7 @@ def oxford_comma(array)
   # puts 'last: '+ last
   str = array.join(', ')
   puts 'str: '+ str
-  str << (array.size > 0 ? " and #{last}" : last)
+  str << (array.size > 1 ? " and #{last}" : last)
 end
 # def oxford_comma(array)
 #   last = array.pop
