e5315d98e51bd6f0c24db7614cbb254e78a47693
sergio marelli <sergio.mrll@gmail.com>
2019-11-03 21:08:02 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,4 +2,4 @@ def oxford_comma(array)
   array.pop
 end
 
-oxford_comma(["kiwi", "durian", "starfruit", "mangos", "dragon fruits"])
+puts oxford_comma(["kiwi", "durian", "starfruit", "mangos", "dragon fruits"])
