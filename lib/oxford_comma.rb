60b7995c3c23a274802d81d302c086399522bbdb
sergio marelli <sergio.mrll@gmail.com>
2019-11-03 21:11:18 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,9 @@
 def oxford_comma(array)
   last = array.pop
   array.join(', ')
+  array
 end
 
 puts oxford_comma(["kiwi", "durian", "starfruit", "mangos", "dragon fruits"])
+puts oxford_comma(["kiwi", "durian"])
+puts oxford_comma(["kiwi"])
